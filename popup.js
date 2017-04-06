80a2e8c9ca9dbde818f92356227965bb7b63bc52
Edgar Guadalupe <edgar.guadalupe@mitechnologiesinc.com>
2017-04-06 15:28:57 -0700

    Add Token and Token Expiration. Close #1

--- popup.js
+++ popup.js
@@ -1,14 +1,32 @@
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function renderData(dataText) {
+  document.getElementById('token').textContent = dataText;
+}
+function renderExpiration(expirationText) {
+  document.getElementById('expiration').textContent = expirationText;
+}
+
+function ReadActiveToken(){
+   $.ajax({
+          url: "http://192.168.0.65:8089/api/v1/activetoken/"
+      }).then(function(data) {
+         renderData(data);
+      });
+}
+
+function ReadExpirationDate(){
+   $.ajax({
+          url: "http://192.168.0.65:8089/api/v1/tokenexpiration/"
+      }).then(function(data) {
+         renderExpiration(data);
+      });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
- 
-    renderStatus('Bin Manager');
-    var imageResult = document.getElementById('image-result');
-    imageResult.src = 'http://apps3.mitechnologiesinc.com/library/images/mit_log.png';
-    imageResult.hidden = false;
 
-   
-}); //addEventListener
+    var imageResult = document.getElementById('image-result');
+    imageResult.src = 'mit_logo.jpg';
+    imageResult.hidden = false;
+    ReadActiveToken();
+    ReadExpirationDate();   
+}); 
