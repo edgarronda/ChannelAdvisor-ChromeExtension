09613335684bf41fe54d8646e635c0e514bc74ed
Edgar Guadalupe <edgar.guadalupe@mitechnologiesinc.com>
2017-04-14 10:44:18 -0700

    Fix image hidden.

--- popup.js
+++ popup.js
@@ -21,9 +21,9 @@ function refreshtoken(e) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('refreshtokenbutton').addEventListener('click', refreshtoken);      
-  document.getElementById('image-result').src = 'assets/mit_logo.jpg';
-    
+  document.getElementById('refreshtokenbutton').addEventListener('click', refreshtoken);
+ document.getElementById('image-result').src = 'assets/mit_logo.jpg';
+  document.getElementById('image-result').hidden = false;
   AjaxCall('token',"http://192.168.0.65:8089/api/v1/token/activetoken/");
   AjaxCall('expiration',"http://192.168.0.65:8089/api/v1/token/tokenexpiration/");
   AjaxCall('previous',"http://192.168.0.65:8089/api/v1/token/previoustoken/");
